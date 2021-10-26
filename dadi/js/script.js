b9aa011b28f187228082ea0b1e2dae7d4c3b7fa4
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-26 16:58:21 +0200

    fine progetto dai e stili

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,12 +2,12 @@
 //2. generare un numero da 1 a 6
 //3. chi ha il numero più alto vince
 
-const randomNum = Math.floor(Math.random()*7);
-console.log ( Math.floor(Math.random()*7));
+const randomNum =Math.floor((Math.random()*6)+1);
+console.log ( Math.floor(Math.random()*6)+1);
 document.getElementById('numero1').innerHTML = "il tuo numero è " + randomNum;
 
-const randomNumComp = Math.floor(Math.random()*7);
-console.log ( Math.floor(Math.random()*7));
+const randomNumComp = Math.floor((Math.random()*6)+1);
+console.log ( Math.floor(Math.random()*6)+1);
 document.getElementById('numero2').innerHTML = "il numero del computer è " + randomNumComp;
 
 if (randomNum > randomNumComp) {
@@ -16,3 +16,4 @@ if (randomNum > randomNumComp) {
 else{
   document.getElementById('risultato').innerHTML = "hai perso"
 }
+
