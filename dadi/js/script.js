7d76ee6086fe4d79db2d2865b7acea6c6d5f7e7f
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-26 16:31:07 +0200

    fine progetto dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,18 @@
+//1. generare al giocatore un numero da 1 a 6
+//2. generare un numero da 1 a 6
+//3. chi ha il numero più alto vince
+
+const randomNum = Math.floor(Math.random()*7);
+console.log ( Math.floor(Math.random()*7));
+document.getElementById('numero1').innerHTML = "il tuo numero è " + randomNum;
+
+const randomNumComp = Math.floor(Math.random()*7);
+console.log ( Math.floor(Math.random()*7));
+document.getElementById('numero2').innerHTML = "il numero del computer è " + randomNumComp;
+
+if (randomNum > randomNumComp) {
+  document.getElementById('risultato').innerHTML = "hai vinto"
+}
+else{
+  document.getElementById('risultato').innerHTML = "hai perso"
+}
